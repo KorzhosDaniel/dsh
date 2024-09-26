15bc73a554690393dfd842d13b9ff0679b2076c7
Korzhos Daniel <daniel.korzhos@yahoo.com>
2024-09-26 17:31:14 +0200

    added exit command, backspace support

--- src/main.go
+++ src/main.go
@@ -13,17 +13,13 @@ func main() {
 
 	pos := map[string]interface{}{
 		"x1": 7,
-		"x2": 8,
 		"y1": 0,
-		"y2": 1,
 	}
 
 	TAG := map[string]interface{}{
 		"label": "[dsh]$",
 		"x1":    0,
-		"x2":    6,
 		"y1":    0,
-		"y2":    1,
 	}
 
 	s, err := tcell.NewScreen()
@@ -41,8 +37,6 @@ func main() {
 	drawText(s,
 		TAG["x1"].(int),
 		TAG["y1"].(int),
-		TAG["x2"].(int),
-		TAG["y2"].(int),
 		defStyle,
 		TAG["label"].(string))
 
@@ -61,70 +55,65 @@ func main() {
 		case *tcell.EventResize:
 			s.Sync()
 		case *tcell.EventKey:
-			if ev.Key() == tcell.KeyEscape || ev.Key() == tcell.KeyCtrlC {
-				quit()
+			if ev.Key() == 127 {
+				if pos["x1"].(int) > 7 {
+					pos["x1"] = pos["x1"].(int) - 1
+
+					buf = buf[:len(buf)-1]
+
+					drawText(s,
+						pos["x1"].(int),
+						pos["y1"].(int),
+						defStyle,
+						" ",
+					)
+				}
 			} else if ev.Key() == tcell.KeyEnter {
 				if buf == "clear" {
 					s.Clear()
 					buf = ""
 
 					TAG["y1"] = -1
-					TAG["y2"] = 0
 
 					pos["y1"] = -1
-					pos["y2"] = 1
 					pos["x1"] = 7
-					pos["x2"] = 8
+				} else if buf == "exit" {
+					quit()
 				} else {
 					buf = ""
 				}
 				// Update column position for dsh tag
 				TAG["y1"] = TAG["y1"].(int) + 1
-				TAG["y2"] = TAG["y2"].(int) + 1
 
 				// Update column and reset row position for cursor
 				pos["y1"] = pos["y1"].(int) + 1
-				pos["y2"] = pos["y2"].(int) + 1
 				pos["x1"] = 7
-				pos["x2"] = 8
 				drawText(s,
 					TAG["x1"].(int),
 					TAG["y1"].(int),
-					TAG["x2"].(int),
-					TAG["x2"].(int),
 					defStyle,
 					TAG["label"].(string),
 				)
-			} else {
+			} else if ev.Rune() >= 32 && ev.Rune() <= 126 {
 				drawText(s,
 					pos["x1"].(int),
 					pos["y1"].(int),
-					pos["x2"].(int),
-					pos["y2"].(int),
 					defStyle,
 					string(rune(ev.Rune())),
 				)
 				pos["x1"] = pos["x1"].(int) + 1
-				pos["x2"] = pos["x2"].(int) + 1
 				buf = buf + string(rune(ev.Rune()))
 			}
 		}
 	}
 }
 
-func drawText(s tcell.Screen, x1, y1, x2, y2 int, style tcell.Style, text string) {
+func drawText(s tcell.Screen, x1, y1 int, style tcell.Style, text string) {
 	row := y1
 	col := x1
 
 	for _, r := range []rune(text) {
 		s.SetContent(col, row, r, nil, style)
 		col++
-		if col >= x2 {
-			row++
-			col = x1
-		}
-		if row >= y2 {
-			break
-		}
 	}
 }
